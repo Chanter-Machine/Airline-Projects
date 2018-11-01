38646fa2cc557fb452d8d90084e793c6fa6a498c
Chaofan <Jarvis@10.100.120.102>
2018-11-01 21:42:46 +0000

    Provide two methods for login verfication, second one returns a json string

--- src/main/java/com/airline/handlers/PassengerController.java
+++ src/main/java/com/airline/handlers/PassengerController.java
@@ -1,19 +1,38 @@
 package com.airline.handlers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.airline.bean.Passenger;
+import com.airline.bean.User;
+import com.airline.services.IUserService;
+import com.airline.utils.Msg;
 
 
 @Controller
 public class PassengerController {
-
+	@Autowired
+	IUserService userService;
 	
 	@RequestMapping("/login.do")
-	public void loginCheck(Passenger p) {
+	public ModelAndView loginCheck(User u) {
 
+		User user = userService.queryUser(u.getEmail(), u.getPassword());
 		
-		System.out.println(p.getPassword());
+		ModelAndView mv = new ModelAndView(); 
+		mv.addObject("user", user);
+		mv.setViewName("/WEB-INF/view/login.jsp");
+		
+		return mv;
 	}
+	
+	@RequestMapping("/login2.do")
+	@ResponseBody
+	public Msg loginCheck2(User u) {
+		User user = userService.queryUser(u.getEmail(), u.getPassword());
+		return Msg.success().add("user", user);
+	}
+	
 }
