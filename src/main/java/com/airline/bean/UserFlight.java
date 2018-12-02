6e2c433b1bf4262be783e0840400c6db934c423d
solaolu <adeolu.akinfemi@yahoo.com>
2018-12-02 11:18:29 +0000

    Get user's flight records

---
+++ src/main/java/com/airline/bean/UserFlight.java
@@ -0,0 +1,11 @@
+package com.airline.bean;
+
+import java.util.Date;
+
+public class UserFlight {
+    Date flightDate;
+    String flightOrigin;
+    String flightDestination;
+    String paymentStatus;
+    Date flightTime;
+}
