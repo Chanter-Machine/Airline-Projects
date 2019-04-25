99ff95e6f54cf85e65d8d9e405f3707aacd7131f
Jude <judeiwuh@gmail.com>
2019-04-25 10:49:26 +0100

    updated Adverts and Places of Interest

--- src/main/java/com/airline/advert/AdvertOriginator.java
+++ src/main/java/com/airline/advert/AdvertOriginator.java
@@ -1,16 +1,29 @@
 package com.airline.advert;
 
+import com.airline.bean.Advert;
+
+import java.util.List;
+
 public class AdvertOriginator implements Originator {
     private int advertid;
     private String description;
     private String category;
     private String imagelocation;
-    public AdvertOriginator(int advertid, String description, String category, String imagelocation) {
-        super();
-        this.advertid = advertid;
+
+   /* public AdvertOriginator(int advertid, String description, String category, String imagelocation) {
+    this.advertid = advertid;
         this.description = description;
         this.category = category;
-        this.imagelocation = imagelocation;
+        this.imagelocation = imagelocation; */
+
+    public AdvertOriginator(List<Advert> adverts) {
+        super();
+        for (Advert ad: adverts) {
+            advertid = ad.getAdvertid();
+            description = ad.getDescription();
+            category = ad.getCategory();
+            imagelocation = ad.getLocation();
+        }
     }
     public int getAdvertid() {
         return advertid;
